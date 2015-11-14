9d0a6ea4bb58b17af0dd463caf2ed9e90669da1e
songshimvp <569303209@qq.com>
2015-11-14 23:18:33 +0800

    Update main.cpp

--- ChineseChess/main.cpp
+++ ChineseChess/main.cpp
@@ -8,9 +8,9 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
 
     QTime t = QTime::currentTime();
-    qsrand(t.msec()+t.second()*1000);//取时间作为产生随机数的种子
+    qsrand(t.msec()+t.second()*1000);  //取时间作为产生随机数的种子
 
-    ChooseDlg dlg;  //选择游戏类型对话框
+    ChooseDlg dlg;    //选择游戏类型对话框
     if(dlg.exec() != QDialog::Accepted)
         return 0;
 
